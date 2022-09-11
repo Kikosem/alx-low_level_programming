ec25409c60634650c103dc5a360a59fd92246f20
Kikosem <kigwubor@gmail.com>
2022-09-11 13:54:53 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	char d;
+
+	for (d = 0; d < 10; d++)
+		putchar(d);
+
+	putchar('\n');
+	return (0);
+}
