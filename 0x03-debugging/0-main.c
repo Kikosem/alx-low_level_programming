322dbef1c7a51f2b91fc7bcbe2c455fccfd494a0
Kikosem <kigwubor@gmail.com>
2022-09-15 07:32:28 -0700

    zero is 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,22 @@
-#include "main.h"
-
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
- * main - testing function
- * Return: 0
+ * main - generates random number
+ * if statements check number
+ * Return: Always 0
  */
-
 int main(void)
 {
+	int n;
 
+	srand(time(0));
+	n = 0;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
 }
