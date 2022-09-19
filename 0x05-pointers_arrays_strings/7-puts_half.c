cb5f66f811ecdbcd421ed661c3804366a8b90b17
Kikosem <kigwubor@gmail.com>
2022-09-19 16:54:48 -0700

    Prints second half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - puts half of int
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int oe = 0;
+
+	if (_strlen(str) % 2 != 0)
+	{
+		oe += 1;
+	}
+	for(i = (_strlen(str) + oe) / 2; i < _strlen(str); i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n);
+}
