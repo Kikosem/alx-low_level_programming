e1849f75662cc46b844d94eff5aedda65a9553d1
Kikosem <kigwubor@gmail.com>
2022-09-19 17:16:08 -0700

    Print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,7 +14,7 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", *(a + i));
-		void print_array(int *a, int n);void print_array(int *a, int n);
+		
 		if (i != n - 1)
 			printf(", ");
 	}
