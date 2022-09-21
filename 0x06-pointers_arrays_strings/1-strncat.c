c63894275ea273f0d5cd0f99e8f017ca07fd10b6
Kikosem <kigwubor@gmail.com>
2022-09-21 15:50:11 -0700

    Concat strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,11 +12,11 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (j = 0; dest[j] != '\0'; j++)
 	{
 	}
 
-	for (j = 0; j < n && src[j] != '\0'; j++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i + j] = src[i];
 	}
