7eee2402ea8bf60c66fe094a2efa7399fcd11f9c
Kikosem <kigwubor@gmail.com>
2022-10-04 06:45:23 -0700

    Add 1-strdup.c file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+/**
+ * _strlen - string length
+ * @s: string in questions
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen(s + 1));
+}
+
+/**
+ * _strdup - returns a pointer to a space in memory containing
+ * the string copy
+ * @str: the string to be copied
+ * Return: pointer to the allocated memory
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	unsigned int size;
+	unsigned int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	size = _strlen(str) + 1;
+
+	s = malloc(sizeof(char) * size);
+
+	if (s == NULL)
+		return (NULL);
+
+	idx = 0;
+	while (idx < size)
+	{
+		s[i] = str[i];
+		i++;
+	}
+
+	return (s);
+}
