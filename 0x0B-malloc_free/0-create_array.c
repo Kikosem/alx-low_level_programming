7b241bfb99f8b0ad4b82337cb43da608246d4a66
Kikosem <kigwubor@gmail.com>
2022-10-04 06:33:00 -0700

    Create an array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+/**
+ * create_array - allocates n bytes in memory
+ * @size: the number of chars to store
+ * @c: the char to insert
+ * Return: pointer to the allocated memory
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int idx;
+
+	if (size == 0)
+		return (0);
+
+	arr = malloc(sizeof(char) * size);
+
+	if (arr == NULL)
+		return (0);
+
+	idx = 0;
+	while (idx < size)
+	{
+		arr[idx] = c;
+		idx++;
+	}
+	return (arr);
+}
